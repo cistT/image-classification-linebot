352de50b6ad5da9ffaf9d9a2e0f190346fc5abe6
cistT <cist.b220.t.chitose@gmail.com>
2023-03-03 15:17:32 +0900

    ユーザ別の生き物の画像を表示

---
+++ src/pages/bingo/[id].tsx
@@ -0,0 +1,54 @@
+import { getUserData } from "@/utiles/creatureModel";
+import { Creature } from "@prisma/client";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+
+const Index = () => {
+  const router = useRouter();
+  const id = router.query.id as string;
+
+  const [creatures, setCreatures] = useState<Creature[]>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`/api/bingo/${id}`);
+      const fetchedUserData = await res.json();
+      if (fetchedUserData) {
+        setCreatures(fetchedUserData.creatures);
+      }
+      setLoading(false);
+    })();
+  }, [id]);
+
+  if (loading) {
+    return <>Loading...</>;
+  }
+
+  if (!creatures) {
+    return <>faild</>;
+  }
+
+  return (
+    <>
+      {creatures.map((creature) => (
+        <div key={creature.name}>
+          <h1>{creature.name}</h1>
+          {creature.imageURL ? (
+            <Image
+              width={300}
+              height={300}
+              src={creature.imageURL}
+              alt={creature.name}
+            />
+          ) : (
+            <>NoImage</>
+          )}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Index;
