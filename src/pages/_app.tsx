00b86bc0bb0f93b26441b176e16821a1cb5a1579
cistT <cist.b220.t.chitose@gmail.com>
2023-03-05 13:21:31 +0900

    ヘッダーを全ページで表示

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
+import Header from "@/components/Header/Header";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />{" "}
+    </>
+  );
 }
